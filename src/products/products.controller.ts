0262c19f0c1d5a84f57a9eb0a115599a48363a7a
cpandares <pandaresocesar@gmail.com>
2025-04-22 12:44:13 -0400

    implement custom RPC exception filter and update product DTOs with error handling in controller

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,10 @@
-import { Body, Controller, Delete, Get, Inject, Param, Patch, Post, Query } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { BadRequestException, Body, Controller, Delete, Get, Inject, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError, firstValueFrom } from 'rxjs';
 import { PaginationDto } from 'src/common/dto/pagination.dto';
 import { PRODUCT_SERVICE } from 'src/config';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 
 @Controller('products')
@@ -11,8 +14,13 @@ export class ProductsController {
   ) {}
 
   @Post()
-  createProduct(){
-    return 'create Product';
+
+   createProduct(@Body() createProductDto: CreateProductDto){
+    try {
+      return this.productsClient.send({ cmd: 'create_product' }, createProductDto)
+    } catch (error) {
+      throw new RpcException(error)
+    }
   }
 
   @Get()
@@ -24,21 +32,48 @@ export class ProductsController {
   }
 
   @Get(':id')
-  findProducts(@Param('id') id:string){
-    return 'this return this product ' + id
+ async findProduct(@Param('id') id:string){
+
+    try {
+      const product = await firstValueFrom(this.productsClient.send({ cmd: 'find_one_product' },{
+        id
+      }))
+
+      return product
+    } catch (error) {
+        throw new RpcException(error)
+    } 
+
+     
   }
 
   @Delete(':id')
   deleteProduct(@Param('id') id:string){
-    return 'this delete product ' + id
+    
+      return this.productsClient.send({ cmd: 'delete_product' },{
+        id
+      }).pipe(
+        catchError( err => {
+          throw new RpcException(err)
+        })
+      )
+    
   }
 
   @Patch(':id')
   updateProduc(
-      @Param('id') id:string,
-      @Body() body : any
+      @Param('id', ParseIntPipe) id:string,
+      @Body() updateProductDto : UpdateProductDto
     ){
-    return 'this update the product ' + id
+     
+      
+      return this.productsClient.send({ cmd: 'update_product' },{ id, ...updateProductDto })
+      .pipe(
+        catchError( err => {
+          throw new RpcException(err)
+        })
+      )
+    
   }
 
 
